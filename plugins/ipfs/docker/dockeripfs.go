5ded3f7be57ca49718e7e3509d86e34299cea420
dgrisham <dgrisham@mines.edu>
2018-09-08 21:22:07 -0600

    working on docker exec issue

--- plugins/ipfs/docker/dockeripfs.go
+++ plugins/ipfs/docker/dockeripfs.go
@@ -231,10 +231,12 @@ func (l *DockerIpfs) RunCmd(ctx context.Context, stdin io.Reader, args ...string
 	if stdin != nil {
 		args = append([]string{"exec", "-i", id}, args...)
 	} else {
-		args = append([]string{"exec", id}, args...)
+		args = append([]string{"exec", "-t", id}, args...)
 	}
+	// fmt.Println(args)
 
 	cmd := exec.CommandContext(ctx, "docker", args...)
+	// cmd := exec.Command("docker", args...)
 	cmd.Stdin = stdin
 
 	stderr, err := cmd.StderrPipe()
